45e1edbe9c02f91e63c1bb95148a32e00bca5da1
behrouzsh <behrouzsharifi7@gmail.com>
2024-12-24 02:04:05 +0330

    signal creation to make an instance in business model after User instance

--- main.py
+++ main.py
@@ -2,8 +2,39 @@ from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
 from models import *
 from authentication import get_hashed_password
+#signals
+from tortoise.signals import post_save
+from typing import List, Optional, Type
+from tortoise import BaseDBAsyncClient
 
 app = FastAPI()
+
+
+
+@post_save(User)
+async def create_business(
+    sender: "Type[User]",
+    instance: User,
+    created: bool,
+    using_db: "Optional[BaseDBAsyncClient]",
+    update_fields: List[str]
+) -> None:
+    if created:
+        business_obj = await Business.create(
+            business_name = instance.username,
+            owner = instance
+        )
+        await business_pydantic.from_tortoise_orm(business_obj)
+        # send the email
+
+
+
+
+
+
+
+
+
 
 @app.post("/registration")
 async def user_registrtion(user: user_pydanticIn):
